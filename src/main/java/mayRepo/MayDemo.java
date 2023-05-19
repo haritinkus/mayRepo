8adc34d261c4f8bf1aeec1741006f1a7d067e97d
Harish Kailasa <hari.kailasa@gmail.com>
2023-05-20 05:10:14 +0530

    Second Commit

--- src/main/java/mayRepo/MayDemo.java
+++ src/main/java/mayRepo/MayDemo.java
@@ -5,6 +5,8 @@ public class MayDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git demo");
+		System.out.println("second commit demo");
+		//System.out.println("Git demo");
 
 	}
 
