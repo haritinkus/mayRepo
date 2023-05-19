d7609c00c813e727e61ef2e37d28601b5fbd6ca5
Harish Kailasa <hari.kailasa@gmail.com>
2023-05-19 05:53:09 +0530

    Myfirst commit

---
+++ src/main/java/mayRepo/MayDemo.java
@@ -0,0 +1,11 @@
+package mayRepo;
+
+public class MayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git demo");
+
+	}
+
+}
---
+++ src/test/java/mayRepo/MayTest.java
@@ -0,0 +1,5 @@
+package mayRepo;
+
+public class MayTest {
+
+}
